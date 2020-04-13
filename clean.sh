cd13e7146250464c39a12a2c124a753196cc663e
David Simon Tetruashvili <david.tetruashvili@kcl.ac.uk>
2020-04-13 05:31:31 +0200

    :see_no_evil: Update .gitignore and compile and clean scripts

--- clean.sh
+++ clean.sh
@@ -1,2 +1,1 @@
 find .  -name "*.c" -type f -delete  -o -name "*.so" -type f -delete
-rm -f ./solved-af
--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 cythonize -i ./saf/framework.py ./saf/io.py ./saf/theories.py ./saf/utils.py ./saf/tasks.py ./saf/argument.py
 cython ./saf/__main__.py --embed -X language_level=3
-gcc -Os -I /usr/include/python3.8 -o solved-af ./saf/__main__.c -lpython3.8 -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python3.8 -o solved-af-static ./saf/__main__.c -lpython3.8 -lpthread -lm -lutil -ldl
+
+./clean.sh
