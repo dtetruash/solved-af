77b121852a7c413b3e4a4a41b424ccbb2bc5a3b5
David Simon Tetruashvili <david.tetruashvili@kcl.ac.uk>
2020-04-13 05:26:34 +0200

    :package: Add compile and clean scripts and update .gi

---
+++ clean.sh
@@ -0,0 +1,2 @@
+find .  -name "*.c" -type f -delete  -o -name "*.so" -type f -delete
+rm -f ./solved-af
---
+++ compile.sh
@@ -0,0 +1,3 @@
+cythonize -i ./saf/framework.py ./saf/io.py ./saf/theories.py ./saf/utils.py ./saf/tasks.py ./saf/argument.py
+cython ./saf/__main__.py --embed -X language_level=3
+gcc -Os -I /usr/include/python3.8 -o solved-af ./saf/__main__.c -lpython3.8 -lpthread -lm -lutil -ldl
