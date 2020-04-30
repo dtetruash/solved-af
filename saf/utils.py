abda79cc8048883bc01aceaebec2ab10dbe91a6d
David Simon Tetruashvili <david.tetruashvili@kcl.ac.uk>
2020-04-30 02:28:05 +0200

    :page_facing_up: Add GPL3 to all source files

--- saf/utils.py
+++ saf/utils.py
@@ -1,3 +1,19 @@
+# Solved-AF -- Copyright (C) 2020  David Simon Tetruashvili
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 def flatten(list_to_flatten):
     # https://stackoverflow.com/a/952952/5065263
     return [item for sublist in list_to_flatten for item in sublist]
